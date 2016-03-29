ea234f981d44a0dbc4eeef2ca1438455a800267b
skkallayath <skkallayath@suyati.com>
2016-03-29 20:11:44 +0530

    The Initial Commit

---
+++ SimpleTokenOAuth/SimpleTokenOAuth/Config/SimpleTokenAuthConfiguration.cs
@@ -0,0 +1,215 @@
+﻿namespace SimpleTokenOAuth.Config
+{
+    using System.Configuration;
+    using System.Web.Configuration;
+
+    /// <summary>
+    /// The Simple Token Auth Configuration
+    /// </summary>
+    public class SimpleTokenAuthConfiguration : ConfigurationSection
+    {
+        /// <summary>
+        /// The private instance
+        /// </summary>
+        private static SimpleTokenAuthConfiguration _instance;
+
+        /// <summary>
+        /// The Singleton Instance
+        /// </summary>
+        public static SimpleTokenAuthConfiguration Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = (SimpleTokenAuthConfiguration)WebConfigurationManager.GetSection("simpleTokenAuth");
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// The Is Enabled
+        /// </summary>
+        [ConfigurationProperty("enabled", DefaultValue = true, IsRequired = false)]
+        public bool IsEnabled
+        {
+            get { return (bool)base["enabled"]; }
+            set { base["enabled"] = value; }
+        }
+
+        /// <summary>
+        /// The Token List
+        /// </summary>
+        [ConfigurationProperty("Tokens", IsDefaultCollection = false)]
+        [ConfigurationCollection(typeof(TokenCollection),
+            AddItemName = "add",
+            ClearItemsName = "clear",
+            RemoveItemName = "remove")]
+        public TokenCollection Tokens
+        {
+            get
+            {
+                return (TokenCollection)base["Tokens"];
+            }
+        }
+    }
+
+    /// <summary>
+    /// The Token Collection
+    /// </summary>
+    public class TokenCollection : ConfigurationElementCollection
+    {
+        /// <summary>
+        /// The Constructor
+        /// </summary>
+        public TokenCollection()
+        {
+
+        }
+
+        /// <summary>
+        /// The Indexer
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public TokenConfig this[int index]
+        {
+            get { return (TokenConfig)BaseGet(index); }
+            set
+            {
+                if (BaseGet(index) != null)
+                {
+                    BaseRemoveAt(index);
+                }
+                BaseAdd(index, value);
+            }
+        }
+
+        /// <summary>
+        /// The add
+        /// </summary>
+        /// <param name="tokenConfig"></param>
+        public void Add(TokenConfig tokenConfig)
+        {
+            BaseAdd(tokenConfig);
+        }
+
+        /// <summary>
+        /// The Clear
+        /// </summary>
+        public void Clear()
+        {
+            BaseClear();
+        }
+
+        /// <summary>
+        /// The CreateNewElemnt
+        /// </summary>
+        /// <returns></returns>
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new TokenConfig();
+        }
+
+        /// <summary>
+        /// The GetElementKey
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((TokenConfig)element).Token;
+        }
+
+        /// <summary>
+        /// The Remove
+        /// </summary>
+        /// <param name="serviceConfig"></param>
+        public void Remove(TokenConfig serviceConfig)
+        {
+            BaseRemove(serviceConfig.Token);
+        }
+
+        /// <summary>
+        /// The Remove At
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveAt(int index)
+        {
+            BaseRemoveAt(index);
+        }
+
+        /// <summary>
+        /// The Remove
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(object key)
+        {
+            BaseRemove(key);
+        }
+
+        /// <summary>
+        /// The GetElementByKey
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public TokenConfig GetElement(object key)
+        {
+            return BaseGet(key) as TokenConfig;
+        }
+    }
+
+    /// <summary>
+    /// The tokenConfig
+    /// </summary>
+    public class TokenConfig : ConfigurationElement
+    {
+        /// <summary>
+        /// The Constructor
+        /// </summary>
+        public TokenConfig() { }
+
+        /// <summary>
+        /// The Constructor
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="allow"></param>
+        public TokenConfig(string token, string info, bool allow)
+        {
+            Info = info;
+            Token = token;
+            IsAllowed = allow;
+        }
+
+        /// <summary>
+        /// The Token
+        /// </summary>
+        [ConfigurationProperty("token", IsRequired = true, IsKey = true)]
+        public string Token
+        {
+            get { return (string)this["token"]; }
+            set { this["token"] = value; }
+        }
+
+        /// <summary>
+        /// The Info
+        /// </summary>
+        [ConfigurationProperty("info", IsRequired = false, IsKey = false)]
+        public string Info
+        {
+            get { return (string)this["info"]; }
+            set { this["info"] = value; }
+        }
+
+        /// <summary>
+        /// The IsAllowed
+        /// </summary>
+        [ConfigurationProperty("allow", DefaultValue = true, IsRequired = false, IsKey = false)]
+        public bool IsAllowed
+        {
+            get { return (bool)this["allow"]; }
+            set { this["allow"] = value; }
+        }
+    }
+}
---
+++ SimpleTokenOAuth/SimpleTokenOAuth/TokenOAuthAttribute.cs
@@ -0,0 +1,60 @@
+﻿namespace SimpleTokenOAuth
+{
+    using Config;
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+
+    /// <summary>
+    /// The token Authorize Attribute
+    /// </summary>
+    public class TokenAuthorizeAttribute : AuthorizeAttribute
+    {
+        /// <summary>
+        /// Over Authorization 
+        /// </summary>
+        /// <param name="actionContext"></param>
+        /// <returns></returns>
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            // Checking wheather the Authorization parameter is pending
+            if (actionContext == null || actionContext.Request == null || actionContext.Request.Headers == null || actionContext.Request.Headers.Authorization == null)
+            {
+                return false;
+            }
+
+            /// Checking whether the scheme is "Token"
+            if (actionContext.Request.Headers.Authorization.Scheme != "Token")
+            {
+                return false;
+            }
+
+            // Getting the access Token value
+            var token = actionContext.Request.Headers.Authorization.Parameter;
+
+            if (string.IsNullOrEmpty(token))
+            {
+                return false;
+            }
+
+            // Getting the config values
+            var config = SimpleTokenAuthConfiguration.Instance;
+
+            // Checking the config and Token Count
+            if (config == null || !config.IsEnabled || config.Tokens == null || config.Tokens.Count == 0)
+            {
+                return false;
+            }
+
+            // Checking for the Mathcing Token
+            var matchingToken = config.Tokens.GetElement(token);
+
+            if (matchingToken == null || !matchingToken.IsAllowed)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
